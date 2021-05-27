3c560b4175a5f424b2bac498b638acd8ed27750f
Ting Becker <tingbecker@gmail.com>
2021-05-26 20:10:58 -0700

    Completed project 1

--- P1-WeSplit/P1-WeSplit/ContentView.swift
+++ P1-WeSplit/P1-WeSplit/ContentView.swift
@@ -14,11 +14,12 @@ struct ContentView: View {
     
     // Properties
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    //    @State private var numberOfPeople = 2
+    @State private var numberOfPeopleAsString = ""
     @State private var tipPercentageSegment = 2
     let tipPercentages = [10,15,20,25,0]
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople)+2
+        let peopleCount = Double(numberOfPeopleAsString) ?? 0+2
         let tipSelection = Double(tipPercentages[tipPercentageSegment])
         let orderAmount = Double(checkAmount) ?? 0
         // math
@@ -29,6 +30,10 @@ struct ContentView: View {
         return perPerson
     }
     
+    var netAmount: Double {
+        return totalPerPerson * (Double(numberOfPeopleAsString) ?? 0+2)
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -36,11 +41,15 @@ struct ContentView: View {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.numberPad)
                     
-                    Picker("Number of People", selection: $numberOfPeople) {
-                        ForEach(2..<10) {
-                            Text("\($0) people")
-                        }
-                    }
+                    // Challange 3
+                    TextField("Number of People", text: $numberOfPeopleAsString)
+                        .keyboardType(.numberPad)
+                    
+                    //                    Picker("Number of People", selection: $numberOfPeople) {
+                    //                        ForEach(2..<10) {
+                    //                            Text("\($0) people")
+                    //                        }
+                    //                    }
                 }
                 
                 Section(header: Text("How much tips to leave")) {
@@ -51,7 +60,13 @@ struct ContentView: View {
                     }.pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section {
+                // Challange 2
+                Section(header: Text("Total $ including tips")) {
+                    Text("$\(netAmount, specifier: "%.2f")" )
+                }
+                
+                // Challange 1
+                Section(header: Text("Amount Per Person")) {
                     // %.2f - any sort of floating point number
                     // %g - removed insignificant zeroes from end of number
                     Text("$\(totalPerPerson, specifier: "%.2f")" )
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 100DaysOfSwiftUI
 SwiftUI course by Paul Hudson
+
+Project Status: Ongoing
+Updated: May 24, 2021
+
+Project 1 - WeSplit: a bill splitting app to show the total amount per person based on the number of people and tip percentage.
+
